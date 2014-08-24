0486a3c856efbe794a7906355ca1f8bdaa1c7da8
frederic nikitin <frederic.nikitin@gmail.com>
2014-08-24 14:53:06 +0200

    fixing indentations

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,17 @@ makeCacheMatrix <- function(x = matrix()) {
 	inverse <- NULL
 
 	set <- function(y) {
-    	x <<- y
-    	inverse <<- NULL
-  	}
-  
-  	get <- function() x
-  	
-  	setinv <- function(inv) inverse <<- inv
-  
-  	getinv <- function() inverse
-  
-  	list(set = set, get = get, setinv = setinv, getinv = getinv)
+		x <<- y
+		inverse <<- NULL
+	}
+	
+	get <- function() x
+	
+	setinv <- function(inv) inverse <<- inv
+	
+	getinv <- function() inverse
+	
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -26,16 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 # then the cacheSolve method should retrieve the inverse from the cache with 
 # a message that notify it.
 cacheSolve <- function(x, ...) {
+
+	inv <- x$getinv()
 	
-    inv <- x$getinv()
-  
-  	if(!is.null(inv)) {
-    	message("getting cached data")
-    	return(inv)
-  	}
-  
-  	data <- x$get()
-  	inv <- solve(data, ...)
-  	x$setinv(inv)
-  	inv
+	if(!is.null(inv)) {
+		message("getting cached data")
+		return(inv)
+	}
+	
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setinv(inv)
+	inv
 }
