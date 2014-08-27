e3ff6285c1d0a3f3938b28dfc58070080b418b08
fnikitin <frederic.nikitin@gmail.com>
2014-08-28 00:07:34 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,23 @@
+## Inverting matrices is a computational intensive task, these two functions 
+## collaborate to cache the inverse of a matrix and if the matrix is unchanged returns the 
+## cached inverted matrix
+
 ## makeCacheMatrix: 
 # This function creates a special "matrix" object that can cache its inverse.
+# It takes a matrix as argument and returns a list of functions
+# accessing the matrix and the inverse of the matrix stored in the local 
+# environment
 makeCacheMatrix <- function(x = matrix()) {
 	
   # cached inverse matrix of x
-  xinv <- NULL
+  # sets the inverse to NULL in the function environment
+  xinv <- NULL 
   
   ## set:
-  # a function that set the matrix data
+  # create a function that set the matrix data
+  # it takes a matrix as argument and stores it in the 
+  # makeCacheMatrix environment (using the <<- operator) it also resets the 
+  # inverse to NULL
   set <- function(y) {
   	x <<- y
   	xinv <<- NULL
@@ -55,7 +66,7 @@ cacheSolve <- function(x, ...) {
     
     # cache the calculated inverse matrix to x
     x$setinv(inv)
-}
+  }
 
   # return inverse matrix 
   inv
